73f89a14a2299933cc1be52ba8e0816eef0fae72
Antonio Vasile <farbenblind@gmail.com>
2025-12-10 15:39:13 +0100

    editable page removed from index

--- apps/web/modules/runtime/pages/homepage/indexCyt.vue
+++ apps/web/modules/runtime/pages/homepage/indexCyt.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full max-w-screen-3xl mx-auto px-[20px] relative">
-    <EditablePage :identifier="'index'" :type="'immutable'" />
+    <!-- <EditablePage :identifier="'index'" :type="'immutable'" /> -->
     <BannerSlider />
     <BhFinder />
   </div>
