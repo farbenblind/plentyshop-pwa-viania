a58f32c729290e25699cbf241669569544da6cb1
Antonio Vasile <farbenblind@gmail.com>
2025-12-19 13:34:13 +0100

    TS Widget relocated to components root

--- apps/web/modules/index.ts
+++ apps/web/modules/index.ts
@@ -87,7 +87,7 @@ export default defineNuxtModule({
     // TrustedShops
     await addComponent({
       name: 'TrustedShops',
-      filePath: resolve('./trustedshops/runtime/components/TrustedShops.vue'),
+      filePath: resolve('./runtime/components/TrustedShops/TrustedShops.vue'),
     });
 
     // SeoTextHome
